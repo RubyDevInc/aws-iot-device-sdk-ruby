fb2c1e4b38ee5ef1904fdf384e9fb6a0f8721fc9
Pierre Goudet <p-goudet@ruby-dev.jp>
2016-09-06 17:27:13 +0900

    Update the shadow_client_delete sample

--- samples/shadow_client_samples/test_shadow_client_delete.rb
+++ samples/shadow_client_samples/test_shadow_client_delete.rb
@@ -1,11 +1,50 @@
 require 'aws_iot_device'
+require 'optparse'
 
-root_ca_path = "/Users/Pierre/certs/root-CA.crt"
-private_key_path = "/Users/Pierre/certs/private.pem.key"
-certificate_path = "/Users/Pierre/certs/certificate.pem.crt"
+options = {}
 
-host = "a2perapdhhaey0.iot.ap-northeast-1.amazonaws.com"
-port = 8883
+OptionParser.new do |opts|
+  opts.banner = "Basic usage basic_greeting.rb -c \"YOUR_CERTIFICATE_PATH\" -k \"YOUR_KEY_FILE_PATH\" -ca \"YOUR_ROOT_CA_PATH -H \"YOUR_ENDPOINT\" -p 8883\n"
+  
+  opts.separator ""
+  opts.separator "Common options"
+  opts.on_tail("-h", "--help", "--usage", "Show this message") do
+    puts opts
+    exit
+  end
+  
+  opts.on("-H", "--host [END_POINT]", "The endpoint where you want to connect") do |host|
+    options[:host] = host
+  end
+  
+  opts.on("-p", "--port [MQTT_PORT]", "The port used for the connection Default is 8883") do |port|
+    options[:port] = port
+  end
+  
+  opts.on("-c", "--cert [CERT_FILE_PATH]", "The path to the certificate file of the private key.") do |cert|
+    options[:cert] = cert
+  end
+  
+  opts.on("-k", "--key [KEY_FILE_PATH]", "The path to private key file that would be used for encryption") do |key|
+    options[:key] = key
+  end
+  
+  opts.on("-a", "--root_ca [ROOT_CA_PATH]", "The path to the authority certification file") do |root_ca|
+    options[:root_ca] = root_ca
+  end
+
+  opts.on("-t", "--thing [THING_NAME]", "The Thing name on which the action would be done") do |thing|
+    options[:things] = thing
+  end
+end.parse!(ARGV)
+
+
+host = options[:host]
+port = options[:port] || 8883
+certificate_path = options[:cert]
+private_key_path = options[:key]
+root_ca_path = options[:root_ca]
+thing = options[:things]
 
 my_shadow_client = AwsIotDevice::MqttShadowClient::ShadowClient.new
 my_shadow_client.configure_endpoint(host, port)
@@ -13,7 +52,7 @@ my_shadow_client.configure_credentials(root_ca_path, private_key_path, certifica
 
 my_shadow_client.connect
 
-cli = my_shadow_client.create_shadow_handler_with_name('TestThing', true)
+my_shadow_client.create_shadow_handler_with_name(thing ,false)
 
 filter_callback = Proc.new do |message|
   puts "Executing the specific callback for topic: #{message.topic}\n##########################################\n"
@@ -23,11 +62,12 @@ n = 1
 
 3.times do
   puts "Start shadow_delete\n"
-  cli.shadow_delete(filter_callback, 5)
+  my_shadow_client.delete_shadow(filter_callback, 5)
   json_payload = '{"state":{"desired":{"property":"RubySDK"}}}'
-  cli.shadow_update(json_payload, nil, 5)
+  my_shadow_client.update_shadow(json_payload, nil, 5)
   n += 1
 end
+
 sleep 5
 
 my_shadow_client.disconnect
