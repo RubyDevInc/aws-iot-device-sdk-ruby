6fb7e424ddb845f4f082d4be111eb2db9189a5bf
Pierre Goudet <p-goudet@ruby-dev.jp>
2017-01-19 17:53:03 +0900

    Add action callback and shadow name on topic builder

--- lib/aws_iot_device/mqtt_shadow_client/shadow_client.rb
+++ lib/aws_iot_device/mqtt_shadow_client/shadow_client.rb
@@ -22,12 +22,12 @@ module AwsIotDevice
         end
       end
 
-      def topic_manager
-        @topic_manager = ShadowTopicManager.new(@mqtt_client)
+      def topic_manager(shadow_name)
+        @topic_manager = ShadowTopicManager.new(@mqtt_client, shadow_name)
       end
 
       def create_shadow_handler_with_name(shadow_name, is_persistent_subscribe=false)
-        topic_manager
+        topic_manager(shadow_name)
         @action_manager = ShadowActionManager.new(shadow_name, @topic_manager, is_persistent_subscribe)
       end
 
@@ -43,12 +43,36 @@ module AwsIotDevice
         @action_manager.shadow_delete(timeout, callback, &block)
       end
 
+      def register_get_callback(callback, &block)
+        @action_manager.register_get_callback(callback, &block)
+      end
+
+      def register_update_callback(callback, &block)
+        @action_manager.register_update_callback(callback, &block)
+      end
+
+      def register_delete_callback(callback, &block)
+        @action_manager.register_delete_callback(callback, &block)
+      end
+
       def register_delta_callback(callback)
         @action_manager.register_shadow_delta_callback(callback)
       end
 
       def remove_delta_callback
         @action_manager.remove_shadow_delta_callback
+      end
+
+      def remove_get_callback
+        @action_manager.remove_get_callback
+      end
+
+      def remove_update_callback
+        @action_manager.remove_update_callback
+      end
+
+      def remove_delete_callback
+        @action_manager.remove_delete_callback
       end
 
       def disconnect
