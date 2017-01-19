89fec81ee73dae5861b3b0be25a9917bd91db876
Pierre Goudet <p-goudet@ruby-dev.jp>
2017-01-19 17:54:45 +0900

    Remove shadow as attribute and make it private variable

--- lib/aws_iot_device/mqtt_shadow_client/shadow_topic_manager.rb
+++ lib/aws_iot_device/mqtt_shadow_client/shadow_topic_manager.rb
@@ -4,9 +4,13 @@ module AwsIotDevice
   module MqttShadowClient
     class ShadowTopicManager
 
-      def initialize(mqtt_manager)
+      def initialize(mqtt_manager, shadow_name)
         if mqtt_manager.nil?
           raise "TopicAction_error: TopicAction should be initialized with a mqtt_manager but was undefined"
+        end
+
+        if shadow_name.nil?
+          raise "TopicAction_error: shadow name is required for TopicBuilder"
         end
         @mqtt_manager = mqtt_manager
         @sub_unsub_mutex = Mutex.new()
@@ -16,6 +20,7 @@ module AwsIotDevice
         end
         @subacked = false
         @unsubacked = false
+        @topic = TopicBuilder.new(shadow_name)
         @timeout = @mqtt_manager.mqtt_operation_timeout_s
       end
 
@@ -23,19 +28,17 @@ module AwsIotDevice
         @mqtt_manager.client_id
       end
 
-      def shadow_topic_publish(shadow_name, shadow_action, payload)
-        topic = TopicBuilder.new(shadow_name, shadow_action)
-        @mqtt_manager.publish(topic.get_topic_general, payload, false, 0)
+      def shadow_topic_publish(action, payload)
+        @mqtt_manager.publish(@topic.get_topic_general(action), payload, false, 0)
       end
 
-      def shadow_topic_subscribe(shadow_name, shadow_action, callback=nil, timeout=@timeout)
+      def shadow_topic_subscribe(action, callback=nil, timeout=@timeout)
         @sub_unsub_mutex.synchronize() {
           @subacked = false
-          topic = TopicBuilder.new(shadow_name, shadow_action)
-          if topic.is_delta?(shadow_action)
-            @mqtt_manager.subscribe(topic.get_topic_delta, 0, callback)
+          if @topic.is_delta?(action)
+            @mqtt_manager.subscribe(@topic.get_topic_delta, 0, callback)
           else
-            @mqtt_manager.subscribe_bunch([topic.get_topic_accepted, 0, callback], [topic.get_topic_rejected, 0, callback])
+           @mqtt_manager.subscribe_bunch([@topic.get_topic_accepted(action), 0, callback], [@topic.get_topic_rejected(action), 0, callback])
           end
           if @mqtt_manager.paho_client?
             ref = Time.now + timeout
@@ -50,14 +53,13 @@ module AwsIotDevice
         @subacked
       end
 
-      def shadow_topic_unsubscribe(shadow_name, shadow_action, timeout=@timeout)
+      def shadow_topic_unsubscribe(action, timeout=@timeout)
         @sub_unsub_mutex.synchronize(){
           @unsubacked = false
-          topic = TopicBuilder.new(shadow_name, shadow_action)
-          if topic.is_delta?(shadow_name)
-            @mqtt_manager.unsubscribe(topic.get_topic_delta)
+          if @topic.is_delta?(action)
+            @mqtt_manager.unsubscribe(@topic.get_topic_delta)
           else
-            @mqtt_manager.unsubscribe_bunch(topic.get_topic_accepted, topic.get_topic_rejected)
+            @mqtt_manager.unsubscribe_bunch(@topic.get_topic_accepted(action), @topic.get_topic_rejected(action))
           end
           if @mqtt_manager.paho_client?
             ref = Time.now + timeout
